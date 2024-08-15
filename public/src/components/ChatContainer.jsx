1a9e3a1aeb795aaf3583dab18993cbff9a90acdb
Tyler Yates <tyler.yates2017@gmail.com>
2024-08-14 20:46:01 -0400

    Updated the recieved chat styling

--- public/src/components/ChatContainer.jsx
+++ public/src/components/ChatContainer.jsx
@@ -174,7 +174,8 @@ const Container = styled.div`
     .recieved {
       justify-content: flex-start;
       .content {
-        background-color: #9900ff20;
+        background-color: white;
+        color: black;
       }
     }
   }
